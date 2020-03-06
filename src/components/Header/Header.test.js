49760b729574b0412d6f30a2594d278b4754dd27
James Nowecki <James.Nowecki@Nology.local>
2020-03-06 09:31:26 +0000

    fixed test

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,10 +1,8 @@
 import React from "react";
 import Header from "./Header";
-
+import { render } from "@testing-library/react";
 describe("Header tests", () => {
-  let component;
-
-  beforeEach(() => {
-    component =
+    it("should render", () => {
+      expect(render(<Header />)).toBeTruthy();
+    });
   })
-});
