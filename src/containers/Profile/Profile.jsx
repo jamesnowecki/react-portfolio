b93a2a3fcab89ada02daef37be53071aae93cbe6
James Nowecki <James.Nowecki@Nology.local>
2020-03-13 17:51:38 +0000

    pregome push

---
+++ src/containers/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Profile.module.scss";
+
+const Profile = () => {
+  return (
+    <>
+      <p>Profile works</p>
+    </>
+  );
+};
+
+export default Profile;
