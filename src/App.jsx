c0e41d9e3755bbe6d0828097b535ab1c1a2f2569
JAMES NOWECKI <JAMESNOWECKI@gmail.com>
2020-03-24 16:42:48 +0000

    wizard fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./App.module.scss";
 import Router from "./containers/Routes";
 import ProfileBar from "./containers/ProfileBar";
+// import { faHome } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const App = () => {
 
--- src/components/CardFront/CardFront.test.js
+++ src/components/CardFront/CardFront.test.js
@@ -1,9 +1,12 @@
 import React from "react";
 import CardFront from "./CardFront";
 import { render } from "@testing-library/react";
+import FakeData from '../../data/projects.js';
+
+const FakeObject = FakeData[0];
 
 describe("CardFront tests", () => {
       it("should render", () => {
-        expect(render(<CardFront />)).toBeTruthy();
+        expect(render(<CardFront project={FakeObject}/>)).toBeTruthy();
       });
     })
