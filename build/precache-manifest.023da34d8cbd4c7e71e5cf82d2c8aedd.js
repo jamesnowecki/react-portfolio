c21737741c4e1b195a171acc76c6de4b4d093694
JAMES NOWECKI <JAMESNOWECKI@gmail.com>
2020-03-24 14:32:26 +0000

    pictures fixed

---
+++ build/precache-manifest.023da34d8cbd4c7e71e5cf82d2c8aedd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1512c2794864b0926d556284a037c9a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccc2e6552bdeddc6d30d",
+    "url": "/static/css/main.5036906b.chunk.css"
+  },
+  {
+    "revision": "0aaa2503ca8e992f88f8",
+    "url": "/static/js/2.6bfc9310.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.6bfc9310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccc2e6552bdeddc6d30d",
+    "url": "/static/js/main.7eeb3801.chunk.js"
+  },
+  {
+    "revision": "2ab5528f29807ec3af41",
+    "url": "/static/js/runtime-main.8b054bcd.js"
+  },
+  {
+    "revision": "f17b24f8a81b9442f149166af4cc4999",
+    "url": "/static/media/strange-eyes.f17b24f8.jpg"
+  },
+  {
+    "revision": "25db1fc88e9bb22faab18ff85f9c2aa9",
+    "url": "/static/media/to-do-list-pic.25db1fc8.jpg"
+  },
+  {
+    "revision": "1ebf5dbd4e607787d30d7c0d33dd5988",
+    "url": "/static/media/vojo-pic.1ebf5dbd.png"
+  }
+]);
